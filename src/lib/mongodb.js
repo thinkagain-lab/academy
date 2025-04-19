b1f25e2fabd2fe884f5e27569037ca77afb88fe2
Arijit Hajra <connectwitharijit@gmail.com>
2025-04-19 15:35:54 +0530

    updated Auth

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import {GridFSBucket} from "mongodb";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -17,16 +16,19 @@ export async function connectToDatabase() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((conn) => {
+    const opts = {
+      bufferCommands: false,
+      maxPoolSize: 10, // Maintain up to 10 socket connections
+      serverSelectionTimeoutMS: 5000, // Select a server after 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       console.log("Connected to MongoDB Atlas");
-      return conn;
+      return mongoose;
     });
   }
   
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
